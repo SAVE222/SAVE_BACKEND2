53c97394faff7856c885ad06b24b8dffa6d476db
sungyeon <csy357@gmail.com>
2022-07-12 17:10:16 +0900

    init setting

---
+++ src/main/java/com/save_backend/SaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.save_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/save_backend/SaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.save_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
