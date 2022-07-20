ff481dab7b670d393a7e31554724ea1b93b2d450
sungyeon <csy357@gmail.com>
2022-07-20 14:48:40 +0900

    fix - resolve confilct

--- src/main/java/com/save_backend/config/response/BaseResponseStatus.java
+++ src/main/java/com/save_backend/config/response/BaseResponseStatus.java
@@ -18,19 +18,23 @@ public enum BaseResponseStatus {
 
     EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
-    USERS_INVALID_USER(false, 2003,"권한이 없는 유저의 접근입니다."),
-    USERS_INACTIVE_USER_ID(false, 2004, "유저 아이디 값을 확인해주세요."),
+    INVALID_USER_JWT(false, 2003,"권한이 없는 유저의 접근입니다."),
+    USERS_EMPTY_USER_ID(false, 2004, "유저 아이디 값을 확인해주세요."),
 
-    USERS_INVALID_PHONE_NUMBER(false, 2010, "전화번호 형식을 확인해주세요."),
+    USERS_EMPTY_PASSWORD(false, 2010, "비밀번호를 입력해주세요."),
+
     USERS_INVALID_PASSWORD(false, 2011, "비밀번호 형식을 확인해주세요."),
-    USERS_INVALID_EMAIL(false, 2012, "이메일 형식을 확인해주세요."),
-    INVALID_GENDER(false,2013,"유효하지 않은 성별값입니다."),
+    USERS_INVALID_PHONE_NUMBER(false, 2012, "전화번호 형식을 확인해주세요."),
+    USERS_INACTIVE_USER_ID(false, 2013, "유저 아이디 값을 확인해주세요."),
+    INVALID_GENDER(false,2014,"유효하지 않은 성별값입니다."),
+        
+    USERS_EMPTY_EMAIL(false, 2020, "이메일을 입력해주세요."),
+    USERS_INVALID_EMAIL(false, 2021, "이메일 형식을 확인해주세요."),
+    USERS_EXISTS_EMAIL(false, 2022,"중복된 이메일입니다."),
+    USERS_EXISTS_PHONE_NUMBER(false, 2023,"중복된 전화번호입니다."),
 
-    USERS_EXISTS_EMAIL(false, 2030,"중복된 이메일입니다."),
-    USERS_EXISTS_PHONE_NUMBER(false, 2031,"중복된 전화번호입니다."),
-
-    POSTS_INVALID_CONTENTS(false, 2040, "내용의 글자수를 확인해주세요."),
-    EMPTY_POST_ID(false, 2041, "게시물 아이디 값을 확인해주세요."),
+    POSTS_INVALID_CONTENTS(false, 2030, "내용의 글자수를 확인해주세요."),
+    EMPTY_POST_ID(false, 2031, "게시물 아이디 값을 확인해주세요."),
 
 
     // 필수 정보 누락
@@ -49,8 +53,7 @@ public enum BaseResponseStatus {
      */
     RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
 
-    DUPLICATED_EMAIL(false, 3010, "중복된 이메일입니다."),
-    FAILED_TO_LOGIN(false, 3011,"없는 아이디거나 비밀번호가 틀렸습니다."),
+    FAILED_TO_LOGIN(false, 3010,"없는 아이디거나 비밀번호가 틀렸습니다."),
 
     MODIFY_FAIL_POST(false, 3020, "게시물 수정에 실패하였습니다."),
     DELETE_FAIL_POST(false, 3021, "게시물 삭제에 실패하였습니다."),
@@ -67,8 +70,18 @@ public enum BaseResponseStatus {
 
 
     PASSWORD_ENCRYPTION_ERROR(false, 4010, "비밀번호 암호화에 실패하였습니다."),
-    PASSWORD_DECRYPTION_ERROR(false, 4011, "비밀번호 복호화에 실패하였습니다.");
+    PASSWORD_DECRYPTION_ERROR(false, 4011, "비밀번호 복호화에 실패하였습니다."),
 
+    /**
+     * 6000 : child 관련 오류
+     */
+    POST_CHILD_EMPTY_USERIDX(false, 6000, "유저 정보를 확인해주세요."),
+    POST_CHILD_EMPTY_NAME(false, 6001, "아동 이름을 입력해주세요."),
+    POST_CHILD_EMPTY_GENDER(false, 6002, "아동 성별을 입력해주세요."),
+    POST_CHILD_EMPTY_AGE(false, 6003, "아동 나이를 입력해주세요."),
+    POST_CHILD_EMPTY_ADDRESS(false, 6004, "아동 주소를 입력해주세요."),
+    POST_CHILD_EMPTY_DETAIL_ADDRESS(false, 6005, "아동 상세주소를 입력해주세요."),
+    NOT_EXIST_USER(false, 6006, "유저가 존재하지 않습니다.");
 
     private final boolean isSuccess;
     private final int code;
