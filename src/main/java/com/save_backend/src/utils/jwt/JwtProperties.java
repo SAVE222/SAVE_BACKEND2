20393b34c1dd3f13feb077179fbb2e9d82e751fa
sungyeon <csy357@gmail.com>
2022-07-28 12:57:44 +0900

    add - jwt settings

---
+++ src/main/java/com/save_backend/src/utils/jwt/JwtProperties.java
@@ -0,0 +1,10 @@
+package com.save_backend.src.utils.jwt;
+
+import static com.save_backend.config.secret.Secret.JWT_SECRET_KEY;
+
+public interface JwtProperties {
+    String SECRET_KEY = JWT_SECRET_KEY;
+    int EXPIRATION_TIME =  1*(1000*60*60*24*365); // 토큰의 만료기간 : 1년
+    String TOKEN_PREFIX = "Bearer "; // 토큰 앞에 붙는 정해진 형식 (마지막에 공백필수)
+    String HEADER_STRING = "X-ACCESS-TOKEN"; // JWT token을 넣을 응답헤더 항목
+}
