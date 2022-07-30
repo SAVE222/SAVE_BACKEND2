1ab0e62327202bd87f02ba0eaf212866b6d20d2d
asas6978 <asas6978@naver.com>
2022-07-31 01:37:24 +0900

    add - getSuspectList

---
+++ src/main/java/com/save_backend/src/get/suspect/model/GetSuspectListRes.java
@@ -0,0 +1,16 @@
+package com.save_backend.src.get.suspect.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GetSuspectListRes {
+
+    private int suspectIdx;
+    private String name;
+    private String gender;
+    private String age;
+    private String address;
+    private String relation;
+}
